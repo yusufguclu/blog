f38015a818faef52a75c793228cface8db22660a
yusufguclu <yguclu010@gmail.com>
2024-12-29 12:59:40 +0300

    Add NextAuth route

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,14 +1,13 @@
-import NextAuth from 'next-auth';
-import GithubProvider from 'next-auth/providers/github';
+import NextAuth from 'next-auth'
+import GithubProvider from 'next-auth/providers/github'
 
 const handler = NextAuth({
   providers: [
     GithubProvider({
-      clientId: process.env.GITHUB_ID!,
-      clientSecret: process.env.GITHUB_SECRET!,
+      clientId: process.env.GITHUB_ID ?? '',
+      clientSecret: process.env.GITHUB_SECRET ?? '',
     }),
   ],
-});
+})
 
-export const GET = handler.GET;
-export const POST = handler.POST;
+export { handler as GET, handler as POST }s
